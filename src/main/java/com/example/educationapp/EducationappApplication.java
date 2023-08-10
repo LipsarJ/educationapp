7c3719e5c869b477e03f1186be733edf1d936a4a
Anthony Lapenok <anton.lapenok@gmail.com>
2023-08-10 10:33:14 +0300

    Java 17, gradle базовые зависимости - spring-web, thymeleaf, lombok

---
+++ src/main/java/com/example/educationapp/EducationappApplication.java
@@ -0,0 +1,13 @@
+package com.example.educationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/educationapp/EducationappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.educationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
