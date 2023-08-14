4064426c3be10f733dab50c6822c5047c7fdcecd
Anthony Lapenok <anton.lapenok@gmail.com>
2023-08-14 15:08:16 +0300

    Создал бин password encoder

--- src/main/java/com/example/educationapp/EducationappApplication.java
+++ src/main/java/com/example/educationapp/EducationappApplication.java
@@ -2,12 +2,20 @@ package com.example.educationapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class EducationappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EducationappApplication.class, args);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
