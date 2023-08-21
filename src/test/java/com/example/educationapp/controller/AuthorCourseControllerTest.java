4dd48d6eb6eb18c9045153b72e6fc19eb86073e9
Anthony Lapenok <anton.lapenok@gmail.com>
2023-08-21 14:15:24 +0300

    Тест с использованием MockMVC для контроллера

--- src/test/java/com/example/educationapp/controller/AuthorCourseControllerTest.java
+++ src/test/java/com/example/educationapp/controller/AuthorCourseControllerTest.java
@@ -6,13 +6,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 public class AuthorCourseControllerTest {
 
@@ -20,66 +24,83 @@ public class AuthorCourseControllerTest {
     private AuthorCourseService authorCourseService;
 
     private AuthorCourseController authorCourseController;
+    private MockMvc mockMvc;
 
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         authorCourseController = new AuthorCourseController(authorCourseService);
+        mockMvc = MockMvcBuilders.standaloneSetup(authorCourseController).build();
     }
 
     @Test
-    public void testGetAllCoursesForAuthor() {
+    public void testGetAllCoursesForAuthor() throws Exception {
         List<CourseDto> expectedCourses = new ArrayList<>();
+        expectedCourses.add(new CourseDto()); // Add some dummy data
+
         when(authorCourseService.getAllCoursesForAuthor()).thenReturn(expectedCourses);
 
-        List<CourseDto> result = authorCourseController.getAllCoursesForAuthor();
+        mockMvc.perform(get("/api/v1/author/courses"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(expectedCourses.size()));
 
-        assertEquals(expectedCourses, result);
         verify(authorCourseService, times(1)).getAllCoursesForAuthor();
     }
 
     @Test
-    public void testCreateCourse() {
+    public void testCreateCourse() throws Exception {
         CourseDto courseDto = new CourseDto();
         when(authorCourseService.createCourse(courseDto)).thenReturn(courseDto);
 
-        CourseDto result = authorCourseController.createCourse(courseDto);
+        mockMvc.perform(post("/api/v1/author/courses")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{}"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").exists());
 
-        assertNotNull(result);
-        assertEquals(courseDto, result);
         verify(authorCourseService, times(1)).createCourse(courseDto);
     }
 
     @Test
-    public void testGetCourse() {
+    public void testGetCourse() throws Exception {
         Long courseId = 1L;
         CourseDto expectedCourseDto = new CourseDto();
         when(authorCourseService.getCourse(courseId)).thenReturn(expectedCourseDto);
 
-        CourseDto result = authorCourseController.getCourse(courseId);
+        mockMvc.perform(get("/api/v1/author/courses/{id}", courseId))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").exists());
 
-        assertEquals(expectedCourseDto, result);
         verify(authorCourseService, times(1)).getCourse(courseId);
     }
 
     @Test
-    public void testUpdateCourse() {
+    public void testUpdateCourse() throws Exception {
         Long courseId = 1L;
         CourseDto courseDto = new CourseDto();
         CourseDto updatedCourseDto = new CourseDto();
         when(authorCourseService.updateCourse(courseId, courseDto)).thenReturn(updatedCourseDto);
 
-        CourseDto result = authorCourseController.updateCourse(courseId, courseDto);
+        mockMvc.perform(put("/api/v1/author/courses/{id}", courseId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{}"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").exists());
 
-        assertEquals(updatedCourseDto, result);
         verify(authorCourseService, times(1)).updateCourse(courseId, courseDto);
     }
 
     @Test
-    public void testDeleteCourse() {
+    public void testDeleteCourse() throws Exception {
         Long courseId = 1L;
 
-        authorCourseController.deleteCourse(courseId);
+        mockMvc.perform(delete("/api/v1/author/courses/{id}", courseId))
+                .andExpect(status().isOk());
 
         verify(authorCourseService, times(1)).deleteCourse(courseId);
     }
