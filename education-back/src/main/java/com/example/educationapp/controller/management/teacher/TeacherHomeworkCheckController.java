c8e3ba9bab427973cfd32660fe9fc61d2efc5d4f
Anthony Lapenok <anton.lapenok@gmail.com>
2023-09-08 18:01:28 +0300

    Добавил тесты и swagger аннотации

--- education-back/src/main/java/com/example/educationapp/controller/management/teacher/TeacherHomeworkCheckController.java
+++ education-back/src/main/java/com/example/educationapp/controller/management/teacher/TeacherHomeworkCheckController.java
@@ -1,9 +1,15 @@
 package com.example.educationapp.controller.management.teacher;
 
+import com.example.educationapp.controlleradvice.ErrorResponse;
 import com.example.educationapp.dto.request.teacher.RequestTeacherCheckHomeworkDto;
 import com.example.educationapp.dto.response.HomeworkDoneInfoPage;
 import com.example.educationapp.dto.response.student.ResponseHomeworkDoneStudentDto;
 import com.example.educationapp.service.management.teacher.TeacherHomeworkCheckService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,6 +24,18 @@ public class TeacherHomeworkCheckController {
     private final TeacherHomeworkCheckService teacherHomeworkCheckService;
 
     @GetMapping("/{id}/lessons/{lessonId}/homeworks/{homeworkTaskId}")
+    @Operation(summary = "Получить информацию о сделанных ДЗ для задачи")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешное получение информации о сделанных ДЗ",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = HomeworkDoneInfoPage.class))),
+            @ApiResponse(responseCode = "404", description = "Урок, задача или ДЗ не найдены",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Введены неверные данные",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)))
+    })
     public HomeworkDoneInfoPage getAllHomeworksDoneForTask(@PathVariable Long id, @PathVariable Long lessonId,
                                                            @PathVariable Long homeworkTaskId,
                                                            Pageable pageable,
@@ -34,6 +52,18 @@ public class TeacherHomeworkCheckController {
     }
 
     @PutMapping("/{id}/lessons/{lessonId}/homeworks/{homeworkTaskId}/{homeworkDoneId}")
+    @Operation(summary = "Установить оценку для выполненного ДЗ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Успешное установление оценки для выполненного ДЗ",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseHomeworkDoneStudentDto.class))),
+            @ApiResponse(responseCode = "404", description = "Урок, задача, выполненное ДЗ или студент не найдены",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Введены неверные данные",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponse.class)))
+    })
     public ResponseHomeworkDoneStudentDto setGradeToHomework (@PathVariable Long id, @PathVariable Long lessonId,
                                                               @PathVariable Long homeworkTaskId, @PathVariable Long homeworkDoneId,
                                                               @RequestBody RequestTeacherCheckHomeworkDto requestTeacherCheckHomeworkDto) {
