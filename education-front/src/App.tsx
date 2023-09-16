5367d7260e6903e93dee27be48a35ce28d371275
Anthony Lapenok <anton.lapenok@gmail.com>
2023-09-16 12:04:29 +0300

    переделал боковую панель

--- education-front/src/App.tsx
+++ education-front/src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import {ChakraProvider, Switch} from '@chakra-ui/react';
+import React, {useState} from 'react';
+import {Box, ChakraProvider, CSSReset, IconButton} from '@chakra-ui/react';
+import {FiMenu} from 'react-icons/fi'
 import {AuthProvider} from './contexts/AuthContext';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Home from "./components/Home";
@@ -9,17 +10,34 @@ import EmployeeSearch from "./components/EmployeeSearch";
 import Sidebar from "./components/Sidebar"
 
 function App() {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     return (
         <ChakraProvider>
             <AuthProvider>
+                <CSSReset/>
                 <BrowserRouter>
-                <Sidebar/>
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="/persons" element={<EmployeeSearch/>}/>
-                        <Route path="/login" element={<Login/>}/>
-                        <Route path="/register" element={<Register/>}/>
-                    </Routes>
+                    <Box display="flex" position="relative">
+                        <Sidebar isSidebarOpen={isSidebarOpen}/>
+                        <IconButton
+                            aria-label="Open Sidebar"
+                            background="none"
+                            position="relative"
+                            top="10px"
+                            left="10px"
+                            zIndex="1001"
+                            _hover={{background: 'none'}}
+                            icon={<FiMenu/>}
+                            onClick={() => {
+                                setIsSidebarOpen(!isSidebarOpen);
+                            }}
+                        />
+                        <Routes>
+                            <Route path="/" element={<Home/>}/>
+                            <Route path="/persons" element={<EmployeeSearch/>}/>
+                            <Route path="/login" element={<Login/>}/>
+                            <Route path="/register" element={<Register/>}/>
+                        </Routes>
+                    </Box>
                 </BrowserRouter>
             </AuthProvider>
         </ChakraProvider>
--- education-front/src/components/Sidebar.tsx
+++ education-front/src/components/Sidebar.tsx
@@ -1,31 +1,17 @@
-import React, { useState } from 'react'
-import {
-    Flex,
-    Text,
-    IconButton,
-    Divider,
-    Avatar,
-    Heading
-} from '@chakra-ui/react'
-import {
-    FiMenu,
-    FiHome,
-    FiLogIn,
-    FiUser,
-    FiGlobe,
-    FiLogOut,
-} from 'react-icons/fi'
-import {IoPeople } from 'react-icons/io5'
+import React from 'react'
+import {Avatar, Divider, Flex, Heading, useMediaQuery} from '@chakra-ui/react'
+import {FiGlobe, FiHome, FiLogIn, FiLogOut,} from 'react-icons/fi'
+import {IoPeople} from 'react-icons/io5'
 import NavItem from './NaviItem'
 import {useAuth} from '../contexts/AuthContext';
 import {instanceAxios} from '../utils/axiosConfig';
-import {NavLink, useNavigate} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
 
-export default function Sidebar() {
-    const [navSize, changeNavSize] = useState("large")
+export default function Sidebar({isSidebarOpen}: { isSidebarOpen: boolean }) {
     const navigate = useNavigate();
 
     const {isAuthenticated, setAuthenticated, setUser, user} = useAuth();
+    const [isMobile] = useMediaQuery('(max-width: 768px)');
 
     const handleLogout = async () => {
         try {
@@ -40,46 +26,36 @@ export default function Sidebar() {
 
     return (
         <Flex
-            pos="sticky"
-            position = "fixed"
-            top = "0"
-            left="5"
-            h="95vh"
-            boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
-            borderRadius={navSize == "small" ? "15px" : "30px"}
-            w={navSize == "small" ? "75px" : "200px"}
-            flexDir="column"
-            justifyContent="space-between"
+            h="100vh"
+            position={isMobile ? "fixed" : "relative"}
+            display={isSidebarOpen ? "flex" : "none"}
+            backgroundColor="rgba(255, 155, 13, 1.0)"
+            width="250px"
+            transition="left 0.3s ease-in-out"
+            zIndex="1000"
+            left={isMobile ? (isSidebarOpen ? '0' : '-250px') : '0'}
         >
             <Flex
                 p="5%"
                 flexDir="column"
                 w="100%"
-                alignItems={navSize == "small" ? "center" : "flex-start"}
+                alignItems="flex-start"
                 as="nav"
             >
-                <IconButton
-                    aria-label="label"
-                    background="none"
-                    mt={5}
-                    _hover={{ background: 'none' }}
-                    icon={<FiMenu />}
-                    onClick={() => {
-                        if (navSize == "small")
-                            changeNavSize("large")
-                        else
-                            changeNavSize("small")
-                    }}
-                />
-                <NavItem navSize={navSize} title="Домашняя страница" icon={FiHome} active={navSize === "large"} description="Домашняя страница" url="/" />
+                <NavItem navSize={isSidebarOpen ? "large" : "small"} title="Домашняя страница" icon={FiHome}
+                         active={isSidebarOpen} description="Домашняя страница" url="/"/>
                 {!isAuthenticated ? (
                     <>
-                        <NavItem navSize={navSize} title="Регистрация" icon={FiGlobe} active={navSize === "large"} description="Регистрация" url="/register" />
-                        <NavItem navSize={navSize} title="Войти" icon={FiLogIn} active={navSize === "large"} description="Вход" url="/login" />
-                        <NavItem navSize={navSize} title="Поиск сотрудников" icon={IoPeople} active={navSize === "large"} description="Поиск сотрудников" url="/persons" />
+                        <NavItem navSize={isSidebarOpen ? "large" : "small"} title="Регистрация" icon={FiGlobe}
+                                 active={isSidebarOpen} description="Регистрация" url="/register"/>
+                        <NavItem navSize={isSidebarOpen ? "large" : "small"} title="Войти" icon={FiLogIn}
+                                 active={isSidebarOpen} description="Вход" url="/login"/>
+                        <NavItem navSize={isSidebarOpen ? "large" : "small"} title="Поиск сотрудников" icon={IoPeople}
+                                 active={isSidebarOpen} description="Поиск сотрудников" url="/persons"/>
                     </>
                 ) : (
-                    <NavItem navSize={navSize} title="Выйти" icon={FiLogOut} active={navSize === "large"} description="Выход" url="" />
+                    <NavItem navSize={isSidebarOpen ? "large" : "small"} title="Выйти" icon={FiLogOut}
+                             active={isSidebarOpen} description="Выход" url=""/>
                 )}
             </Flex>
 
@@ -87,14 +63,15 @@ export default function Sidebar() {
                 p="5%"
                 flexDir="column"
                 w="100%"
-                alignItems={navSize == "small" ? "center" : "flex-start"}
+                alignItems={isSidebarOpen ? "center" : "flex-start"} // Изменили navSize на isSidebarOpen
                 mb={4}
             >
-                <Divider display={navSize == "small" ? "none" : "flex"} />
+                <Divider display={isSidebarOpen ? "flex" : "none"}/> {/* Изменили navSize на isSidebarOpen */}
                 {user && (
                     <Flex mt={4} align="center">
-                        <Avatar size="sm" />
-                        <Flex flexDir="column" ml={4} display={navSize === "small" ? "none" : "flex"}>
+                        <Avatar size="sm"/>
+                        <Flex flexDir="column" ml={4}
+                              display={isSidebarOpen ? "flex" : "none"}> {/* Изменили navSize на isSidebarOpen */}
                             <Heading as="h3" size="sm">
                                 {user.username}
                             </Heading>
