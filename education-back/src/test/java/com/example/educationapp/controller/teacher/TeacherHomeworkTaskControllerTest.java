b9f83e768ffd387bfc50f0672a73e6e8214d5c2c
Anthony Lapenok <anton.lapenok@gmail.com>
2023-10-04 13:55:10 +0300

    Update TeacherHomeworkTaskControllerTest.java

--- education-back/src/test/java/com/example/educationapp/controller/teacher/TeacherHomeworkTaskControllerTest.java
+++ education-back/src/test/java/com/example/educationapp/controller/teacher/TeacherHomeworkTaskControllerTest.java
@@ -31,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @WithMockUser(username = "Lipsar", authorities = "TEACHER")
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class TeacherHomeworkTaskControllerTest {
 
     @MockBean
