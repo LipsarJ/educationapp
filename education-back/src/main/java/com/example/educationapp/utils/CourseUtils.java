dccca8884c5fd3fa7feb51cf729f5f22b1e4a0a5
Anthony Lapenok <anton.lapenok@gmail.com>
2023-10-19 23:17:39 +0300

    Update CourseUtils.java

--- education-back/src/main/java/com/example/educationapp/utils/CourseUtils.java
+++ education-back/src/main/java/com/example/educationapp/utils/CourseUtils.java
@@ -66,9 +66,10 @@ public class CourseUtils {
         User user = userRepo.findById(responseUserDto.getId()).orElseThrow(() -> new UserNotFoundException("User not found"));
         Course course = courseRepo.findById(id).orElseThrow(() -> new CourseNotFoundException("Course is not found."));
         if(user.getStudentCourseSet().contains(course) || user.getTeacherCourseSet().contains(course) || user.getTeacherCourseSet().contains(course)){
+            return course;
+        } else {
             throw new ForbiddenException("You are not allowed to this course.");
         }
-        return course;
     }
 
 
