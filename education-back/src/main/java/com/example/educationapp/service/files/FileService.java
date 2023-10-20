4b7653416d5a7ba6e66f2feadc1173468d508fea
Anthony Lapenok <anton.lapenok@gmail.com>
2023-10-20 16:01:08 +0300

    перенёс всё в 1 контроллер и добавил preAuthorize

--- education-back/src/main/java/com/example/educationapp/service/files/FileService.java
+++ education-back/src/main/java/com/example/educationapp/service/files/FileService.java
@@ -7,6 +7,7 @@ import com.example.educationapp.exception.NotFoundException;
 import com.example.educationapp.mapper.MediaMapper;
 import com.example.educationapp.repo.*;
 import com.example.educationapp.security.service.UserContext;
+import com.example.educationapp.utils.HomeworkUtils;
 import com.example.educationapp.utils.LessonUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,9 +17,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -32,11 +33,12 @@ public class FileService {
     private final MediaMapper mediaMapper;
     private final MinioService minioService;
     private final LessonUtils lessonUtils;
+    private final HomeworkUtils homeworkUtils;
     private final UserContext userContext;
     private final UserRepo userRepo;
 
 
-    public UploadFileResDto uploadFile(Long courseId, MultipartFile file, Long fileId, Long id, String mediaOwner) {
+    public UploadFileResDto uploadFileLesson(Long courseId, Long lessonId, MultipartFile file, Long fileId) {
         String object = UUID.randomUUID().toString();
         String fileName = file.getOriginalFilename();
         UploadFileResDto uploadFileResDto = new UploadFileResDto();
@@ -46,74 +48,150 @@ public class FileService {
         } catch (Exception ex) {
             throw new RuntimeException(ex.getMessage());
         }
-        switch (mediaOwner) {
-            case "LESSON" -> {
-                Lesson lesson = lessonUtils.getLessonForAuthorValidatedCourse(courseId, id);
-                MediaLesson mediaLesson = new MediaLesson();
-                mediaLesson.setMediaLesson(lesson);
-                mediaLesson.setFileKey(object);
-                mediaLesson.setMediaType(MediaType.DOCUMENT);
-                mediaLesson.setSize(file.getSize());
-                mediaLesson.setName(fileName);
-                mediaLessonRepo.save(mediaLesson);
-                lesson.getMediaLessonList().add(mediaLesson);
-            }
-            case "HOMEWORK_TASK" -> {
-                HomeworkTask homeworkTask = homeworkTaskRepo.findById(id).orElseThrow(() -> new NotFoundException("Homework Task is not found"));
-                if (!homeworkTask.getLesson().getLessonsCourse().getAuthors().contains(userRepo.findById(userContext.getUserDto().getId()))) {
-                    throw new ForbiddenException("You are not author of this course");
-                }
-                MediaHomeworkTask mediaHomeworkTask = new MediaHomeworkTask();
-                mediaHomeworkTask.setTaskMedia(homeworkTask);
-                mediaHomeworkTask.setFileKey(object);
-                mediaHomeworkTask.setMediaType(MediaType.DOCUMENT);
-                mediaHomeworkTask.setSize(file.getSize());
-                mediaHomeworkTask.setName(fileName);
-                mediaHomeworkTaskRepo.save(mediaHomeworkTask);
-                homeworkTask.getMediaHomeworkTaskList().add(mediaHomeworkTask);
-            }
-            case "HOMEWORK_DONE" -> {
-                HomeworkDone homeworkDone = homeworkDoneRepo.findById(id).orElseThrow(() -> new NotFoundException("HomeworkDone is not found"));
-                if (!homeworkDone.getTask().getLesson().getLessonsCourse().getStudents().contains(homeworkDone.getStudent())) {
-                    throw new ForbiddenException("You are not student of this course");
-                }
-                MediaHomeworkDone mediaHomeworkDone = new MediaHomeworkDone();
-                mediaHomeworkDone.setHomeworkDone(homeworkDone);
-                mediaHomeworkDone.setFileKey(object);
-                mediaHomeworkDone.setMediaType(MediaType.DOCUMENT);
-                mediaHomeworkDone.setSize(file.getSize());
-                mediaHomeworkDone.setName(fileName);
-                mediaHomeworkDoneRepo.save(mediaHomeworkDone);
-                homeworkDone.getMediaHomeworkDoneList().add(mediaHomeworkDone);
-            }
-        }
+        Lesson lesson = lessonUtils.getLessonForAuthorValidatedCourse(courseId, lessonId);
+        MediaLesson mediaLesson = new MediaLesson();
+        mediaLesson.setMediaLesson(lesson);
+        mediaLesson.setFileKey(object);
+        mediaLesson.setMediaType(MediaType.DOCUMENT);
+        mediaLesson.setSize(file.getSize());
+        mediaLesson.setName(fileName);
+        mediaLessonRepo.save(mediaLesson);
+        lesson.getMediaLessonList().add(mediaLesson);
         uploadFileResDto.setFileKey(object.getBytes());
         uploadFileResDto.setName(fileName);
 
         return uploadFileResDto;
     }
 
-    public UploadFileResDto downloadFile(Long courseId, UUID id, String mediaOwner) throws IOException {
-        String fileKey = "";
-        String originalFileName = "";
-        lessonUtils.validateAllUsersForCourse(courseId);
-        switch (mediaOwner) {
-            case "LESSON" -> {
-                MediaLesson mediaLesson = mediaLessonRepo.findById(id).orElseThrow(() -> new NotFoundException("File is not found"));
-                fileKey = mediaLesson.getFileKey();
-                originalFileName = mediaLesson.getName();
-            }
-            case "HOMEWORK_TASK" -> {
-                MediaHomeworkTask mediaHomeworkTask = mediaHomeworkTaskRepo.findById(id).orElseThrow(() -> new NotFoundException("File is not found"));
-                fileKey = mediaHomeworkTask.getFileKey();
-                originalFileName = mediaHomeworkTask.getName();
-            }
-            case "HOMEWORK_DONE" -> {
-                MediaHomeworkDone mediaHomeworkDone = mediaHomeworkDoneRepo.findById(id).orElseThrow(() -> new NotFoundException("File is not found"));
-                fileKey = mediaHomeworkDone.getFileKey();
-                originalFileName = mediaHomeworkDone.getName();
-            }
+    public UploadFileResDto uploadFileHomeworkTask(Long courseId, Long homeworkTaskId, Long lessonId, MultipartFile file, Long fileId) {
+        String object = UUID.randomUUID().toString();
+        String fileName = file.getOriginalFilename();
+        UploadFileResDto uploadFileResDto = new UploadFileResDto();
+        try (InputStream inputStream = file.getInputStream()) {
+            minioService.createBucketIfNotExists();
+            minioService.uploadFile(object, inputStream);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage());
         }
+        HomeworkTask homeworkTask = homeworkUtils.getHomeworkTaskForAuthorValidatedLesson(courseId, lessonId, homeworkTaskId);
+        if (!homeworkTask.getLesson().getLessonsCourse().getAuthors().contains(userRepo.findById(userContext.getUserDto().getId()))) {
+            throw new ForbiddenException("You are not author of this course");
+        }
+        MediaHomeworkTask mediaHomeworkTask = new MediaHomeworkTask();
+        mediaHomeworkTask.setTaskMedia(homeworkTask);
+        mediaHomeworkTask.setFileKey(object);
+        mediaHomeworkTask.setMediaType(MediaType.DOCUMENT);
+        mediaHomeworkTask.setSize(file.getSize());
+        mediaHomeworkTask.setName(fileName);
+        mediaHomeworkTaskRepo.save(mediaHomeworkTask);
+        homeworkTask.getMediaHomeworkTaskList().add(mediaHomeworkTask);
+        uploadFileResDto.setFileKey(object.getBytes());
+        uploadFileResDto.setName(fileName);
+
+        return uploadFileResDto;
+    }
+
+    public UploadFileResDto uploadFileHomeworkDone(Long courseId, Long lessonId, Long homeworkTaskId, MultipartFile file, Long fileId) {
+        String object = UUID.randomUUID().toString();
+        String fileName = file.getOriginalFilename();
+        UploadFileResDto uploadFileResDto = new UploadFileResDto();
+        try (InputStream inputStream = file.getInputStream()) {
+            minioService.createBucketIfNotExists();
+            minioService.uploadFile(object, inputStream);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage());
+        }
+        HomeworkDone homeworkDone = homeworkUtils.getHomeworkDoneForStudent(courseId, lessonId, homeworkTaskId);
+        if (!homeworkDone.getTask().getLesson().getLessonsCourse().getStudents().contains(homeworkDone.getStudent())) {
+            throw new ForbiddenException("You are not student of this course");
+        }
+        MediaHomeworkDone mediaHomeworkDone = new MediaHomeworkDone();
+        mediaHomeworkDone.setHomeworkDone(homeworkDone);
+        mediaHomeworkDone.setFileKey(object);
+        mediaHomeworkDone.setMediaType(MediaType.DOCUMENT);
+        mediaHomeworkDone.setSize(file.getSize());
+        mediaHomeworkDone.setName(fileName);
+        mediaHomeworkDoneRepo.save(mediaHomeworkDone);
+        homeworkDone.getMediaHomeworkDoneList().add(mediaHomeworkDone);
+        uploadFileResDto.setFileKey(object.getBytes());
+        uploadFileResDto.setName(fileName);
+
+        return uploadFileResDto;
+    }
+
+    public UploadFileResDto downloadFileLesson(Long courseId, UUID id) throws IOException {
+        lessonUtils.validateAllUsersForCourse(courseId);
+        MediaLesson mediaLesson = mediaLessonRepo.findById(id).orElseThrow(() -> new NotFoundException("File is not found"));
+        String fileKey = mediaLesson.getFileKey();
+        String originalFileName = mediaLesson.getName();
+        return downloadAndPrepareResponse(fileKey, originalFileName);
+    }
+
+    public UploadFileResDto downloadFileHomeworkTask(Long courseId, UUID id) throws IOException {
+        lessonUtils.validateAllUsersForCourse(courseId);
+        MediaHomeworkTask mediaHomeworkTask = mediaHomeworkTaskRepo.findById(id).orElseThrow(() -> new NotFoundException("File is not found"));
+        String fileKey = mediaHomeworkTask.getFileKey();
+        String originalFileName = mediaHomeworkTask.getName();
+        return downloadAndPrepareResponse(fileKey, originalFileName);
+    }
+
+    public UploadFileResDto downloadFileHomeworkDone(Long courseId, UUID id) throws IOException {
+        lessonUtils.validateAllUsersForCourse(courseId);
+        MediaHomeworkDone mediaHomeworkDone = mediaHomeworkDoneRepo.findById(id).orElseThrow(() -> new NotFoundException("File is not found"));
+        String fileKey = mediaHomeworkDone.getFileKey();
+        String originalFileName = mediaHomeworkDone.getName();
+        return downloadAndPrepareResponse(fileKey, originalFileName);
+    }
+
+    public void deleteFileLesson(Long courseId, Long lessonId, UUID id) {
+        lessonUtils.getLessonForAuthorValidatedCourse(courseId, lessonId);
+        MediaLesson mediaLesson = mediaLessonRepo.findById(id).orElseThrow(() -> new NotFoundException("Media is not found"));
+        deleteMediaFile(mediaLesson.getFileKey());
+        mediaLessonRepo.delete(mediaLesson);
+    }
+
+    public void deleteFileHomeworkTask(Long courseId, Long lessonId, Long homeworkTaskId, UUID id) {
+        homeworkUtils.getHomeworkTaskForAuthorValidatedLesson(courseId, lessonId, homeworkTaskId);
+        MediaHomeworkTask mediaHomeworkTask = mediaHomeworkTaskRepo.findById(id).orElseThrow(() -> new NotFoundException("Homework task is not found"));
+        deleteMediaFile(mediaHomeworkTask.getFileKey());
+        mediaHomeworkTaskRepo.delete(mediaHomeworkTask);
+    }
+
+    public void deleteFileHomeworkDone(Long courseId, Long lessonId, Long homeworkTaskId, UUID id) {
+        homeworkUtils.getHomeworkDoneForStudent(courseId, lessonId, homeworkTaskId);
+        MediaHomeworkDone mediaHomeworkDone = mediaHomeworkDoneRepo.findById(id).orElseThrow(() -> new NotFoundException("Homework done is not found"));
+        deleteMediaFile(mediaHomeworkDone.getFileKey());
+        mediaHomeworkDoneRepo.delete(mediaHomeworkDone);
+    }
+
+    public List<UploadFileResDto> getAllFilesLesson(Long courseId, Long lessonId) {
+        lessonUtils.validateAllUsersForCourse(courseId);
+        Lesson lesson = lessonRepo.findById(lessonId).orElseThrow(() -> new NotFoundException("Lesson is not found"));
+        return lesson.getMediaLessonList()
+                .stream()
+                .map(mediaMapper::toLessonDto)
+                .collect(Collectors.toList());
+    }
+
+    public List<UploadFileResDto> getAllFilesHomeworkTask(Long courseId, Long homeworkTaskId) {
+        lessonUtils.validateAllUsersForCourse(courseId);
+        HomeworkTask homeworkTask = homeworkTaskRepo.findById(homeworkTaskId).orElseThrow(() -> new NotFoundException("Homework Task is not found"));
+        return homeworkTask.getMediaHomeworkTaskList()
+                .stream()
+                .map(mediaMapper::toHomeworkTaskDto)
+                .collect(Collectors.toList());
+    }
+
+    public List<UploadFileResDto> getAllFilesHomeworkDone(Long courseId, Long homeworkDoneId) {
+        lessonUtils.validateAllUsersForCourse(courseId);
+        HomeworkDone homeworkDone = homeworkDoneRepo.findById(homeworkDoneId).orElseThrow(() -> new NotFoundException("HomeworkDone is not found"));
+        return homeworkDone.getMediaHomeworkDoneList()
+                .stream()
+                .map(mediaMapper::toHomeworkDoneDto)
+                .collect(Collectors.toList());
+    }
+
+    private UploadFileResDto downloadAndPrepareResponse(String fileKey, String originalFileName) throws IOException {
         try (InputStream response = minioService.downloadFile(fileKey)) {
             return new UploadFileResDto(
                     URLEncoder.encode(originalFileName, StandardCharsets.UTF_8),
@@ -124,61 +202,11 @@ public class FileService {
         }
     }
 
-    public void deleteFile(Long courseId, Long ownerId, UUID id, String mediaOwner) {
-        String fileKey = "";
-        switch (mediaOwner) {
-            case "LESSON" -> {
-                lessonUtils.getLessonForAuthorValidatedCourse(courseId, ownerId);
-                MediaLesson mediaLesson = mediaLessonRepo.findById(id).orElseThrow(() -> new NotFoundException("Media is not found"));
-                fileKey = mediaLesson.getFileKey();
-                mediaLessonRepo.delete(mediaLesson);
-            }
-            case "HOMEWORK_TASK" -> {
-                HomeworkTask homeworkTask = homeworkTaskRepo.findById(ownerId).orElseThrow(() -> new NotFoundException("Homework Task is not found"));
-                if (!homeworkTask.getLesson().getLessonsCourse().getAuthors().contains(userRepo.findById(userContext.getUserDto().getId()))) {
-                    throw new ForbiddenException("You are not author of this course");
-                }
-                MediaHomeworkTask mediaHomeworkTask = mediaHomeworkTaskRepo.findById(id).orElseThrow(() -> new NotFoundException("Homework task is not found"));
-                fileKey = mediaHomeworkTask.getFileKey();
-                mediaHomeworkTaskRepo.delete(mediaHomeworkTask);
-            }
-            case "HOMEWORK_DONE" -> {
-                HomeworkDone homeworkDone = homeworkDoneRepo.findById(ownerId).orElseThrow(() -> new NotFoundException("HomeworkDone is not found"));
-                if (!homeworkDone.getTask().getLesson().getLessonsCourse().getStudents().contains(homeworkDone.getStudent())) {
-                    throw new ForbiddenException("You are not student of this course");
-                }
-                MediaHomeworkDone mediaHomeworkDone = mediaHomeworkDoneRepo.findById(id).orElseThrow(() -> new NotFoundException("Homework done is not found"));
-                fileKey = mediaHomeworkDone.getFileKey();
-                mediaHomeworkDoneRepo.delete(mediaHomeworkDone);
-            }
-        }
+    private void deleteMediaFile(String fileKey) {
         try {
             minioService.deleteFile(fileKey);
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
         }
-    }
-
-    public List<UploadFileResDto> getAllFiles(Long courseId, Long id, String mediaOwner) {
-        List<UploadFileResDto> files = new ArrayList<>();
-        lessonUtils.validateAllUsersForCourse(courseId);
-        switch (mediaOwner) {
-            case "LESSON" -> {
-                files.addAll(lessonRepo.findById(id).
-                        orElseThrow(() -> new NotFoundException("Lesson is not found"))
-                        .getMediaLessonList().stream().map(mediaMapper::toLessonDto).toList());
-            }
-            case "HOMEWORK_TASK" -> {
-                files.addAll(homeworkTaskRepo.findById(id).
-                        orElseThrow(() -> new NotFoundException("Lesson is not found"))
-                        .getMediaHomeworkTaskList().stream().map(mediaMapper::toHomeworkTaskDto).toList());
-            }
-            case "HOMEWORK_DONE" -> {
-                files.addAll(homeworkDoneRepo.findById(id).
-                        orElseThrow(() -> new NotFoundException("Lesson is not found"))
-                        .getMediaHomeworkDoneList().stream().map(mediaMapper::toHomeworkDoneDto).toList());
-            }
-        }
-        return files;
     }
 }
